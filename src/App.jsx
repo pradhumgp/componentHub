02d4d6972772802d94a382d33203611c37b1e727
pradhumgp <pradhumgp@gmail.com>
2024-05-30 23:08:52 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+
+
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className="h-screen w-screen bg-black flex flex-col items-center p-24">
+        <p className="text-white font-extrabold text-lg">this is a react component hub for creating and storing logic/code for different components. </p>
+      </div>
     </>
   );
 }
