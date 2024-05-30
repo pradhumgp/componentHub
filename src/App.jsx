602a28295a4446e5069257b61b553f763b88626b
pradhumgp <pradhumgp@gmail.com>
2024-05-30 23:34:42 +0530

    font added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
-
 function App() {
   return (
     <>
-      <div className="h-screen w-screen bg-black flex flex-col items-center p-24">
-        <p className="text-white font-extrabold text-lg">this is a react component hub for creating and storing logic/code for different components. </p>
+      <div className="h-screen w-screen bg-gray-900 flex flex-col items-center">
+        <p className="text-white text-2xl font-poppins p-12">
+          This is a react component hub for creating and storing logic/code for
+          different components.
+        </p>
       </div>
     </>
   );
