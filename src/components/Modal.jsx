4f2e8d70dd403b438cdc6854bdc637fba0c9c4b4
pradhumgp <pradhumgp@gmail.com>
2024-06-01 22:10:20 +0530

    modal basic complete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,24 @@
-
-
-const Modal = () => {
+const Modal = ({ show, onClose }) => {
   return (
-    <div>Modal component comes hereeeeeeeeeeeeeeeeee</div>
-  )
-}
+    <>
+      {show && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50" onClick={onClose}>
+          <div
+            className="relative p-6 bg-white rounded shadow-lg"
+          >
+            <button
+              onClick={onClose}
+              className="absolute text-gray-600 top-2 right-2 hover:text-gray-900"
+            >
+              &times;
+            </button>
+            <div>modal body hereeeeee</div>
+          </div>
+        </div>
+      )}
+      <div>{show ? "true" : "false"}</div>
+    </>
+  );
+};
 
-export default Modal
+export default Modal;
--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -1,11 +1,23 @@
-import Modal from "../components/Modal"
+import Modal from "../components/Modal";
+import { useState } from "react";
 
 const ModalPage = () => {
+  const [show, setShow] = useState(false);
+  const toggleModal = () => {
+    setShow(!show);
+  };
+
   return (
     <>
-    <Modal/>
+      <button
+        onClick={toggleModal}
+        className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+      >
+        click to open/close
+      </button>
+      <Modal show={show} onClose={toggleModal} />
     </>
-  )
-}
+  );
+};
 
-export default ModalPage
+export default ModalPage;
